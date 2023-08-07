d04d33ffee27560ca8cfc77287175e3c54636eae
rdheadyii <rdheadyii@gmail.com>
2023-08-07 08:02:39 -0400

    wrote get for all products and sync for Sequelize on server start

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: {include: ['category_name']}
+        },
+        {
+          model: Tag,
+          attributes: {include: ['id', 'tag_name']}
+        }
+      ]
+    })
+    res.status(200).json(productData)
+  } catch(err) {
+    res.status(400).json(err)
+  }
 });
 
 // get one product
